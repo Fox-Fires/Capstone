791f94c4fd0bf3621790898031c7b015af3972df
Mike Koshakow <mzkosh@gmail.com>
2020-08-18 17:04:12 -0400

    teed up

---
+++ client/index.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>Lets play some golf!</h1>, document.getElementById('app'));
---
+++ server/index.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const PORT = process.env.PORT || 1337;
+const app = express();
+const socketio = require('socket.io');
+module.exports = app;
+
+app.use(morgan('dev'));
+
+// body parsing middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found');
+    err.status = 404;
+    next(err);
+  } else {
+    next();
+  }
+});
+
+// sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+});
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
+
+const server = app.listen(PORT, () =>
+  console.log(`Teeing off on port ${PORT}`)
+);
+
+// set up our socket control center
+//  const io = socketio(server)
+//  require('./socket')(io)
