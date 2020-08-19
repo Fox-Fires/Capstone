d98a3a3a699e21a9fae56bf038b92815f60820da
Kevin Moy MAC <kmoy2014@gmail.com>
2020-08-19 13:01:19 -0400

    added localstorage to remember user-form

---
+++ client/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Root } from './components'
+
+const App = () =>{
+  return(
+    <div>
+      <Root />
+    </div>
+  )
+}
+
+export default App
---
+++ client/components/User-form.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const UserForm = props => {
+  return (
+    <div className ="user-form">
+      <label htmlFor="name">Pick a name!</label>
+      <input
+        name = "name"
+        type = "text"
+        value = {props.name}
+        onChange = {props.handleChange}
+      />
+      <button>Play</button>
+    </div>
+  )
+}
+
+export default UserForm
